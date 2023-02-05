9528fc6585bfab3f6e0c7c2407e7a52fc00fa87c
Kira Corbett <kiracorbett00@gmail.com>
2023-02-05 15:33:42 -0800

    Iterate for multiple images

--- image-generator/app.py
+++ image-generator/app.py
@@ -10,18 +10,26 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 print("Loading...")
 
-# response = openai.Image.create(
-#   prompt="vintage raccoon listening to music",
+response = openai.Image.create(
+  prompt="animal tattoo ideas and inspiration",
+  n=5,
+  size="1024x1024"
+)
+
+items = response['data']
+
+for item in items:
+  url = item['url']
+  print(url)
+
+# response = openai.Image.create_variation(
+#   image=open("pyramid.png", "rb"),
 #   n=1,
 #   size="1024x1024"
 # )
 # image_url = response['data'][0]['url']
 # print(image_url)
 
-response = openai.Image.create_variation(
-  image=open("pyramid.png", "rb"),
-  n=1,
-  size="1024x1024"
-)
-image_url = response['data'][0]['url']
-print(image_url)
+# themes
+# vintage animals
+# melancholy songs
