7d800c64790ed2ee523687b5dfc66680af7552dc
Kira Corbett <kiracorbett00@gmail.com>
2023-02-04 23:43:32 -0800

    Inital story generation with prompt

--- ad-generator/app.py
+++ ad-generator/app.py
@@ -6,10 +6,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-#print(os.getenv("OPENAI_API_KEY"))
-#openai.api_key = os.environ["OPENAI_API_KEY"]
 
-response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
+print("Loading...\n")
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Write a creative story for the following animal aimed at people adopting it:\n\nAnimal: Raccoon",
+  temperature=0.5,
+  max_tokens=100,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
 
 result=response.choices[0].text
 print(result)
