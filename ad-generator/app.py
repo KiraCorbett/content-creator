b903e86b7fdcb8cf8eb342ee5a95ef483a7b7854
Kira Corbett <kiracorbett00@gmail.com>
2023-02-05 11:29:01 -0800

    wip: ad generator

--- ad-generator/app.py
+++ ad-generator/app.py
@@ -7,11 +7,16 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-print("Loading...\n")
+user_prompt = input("Enter a prompt: ")
+#user_temp = input("Enter a temperature (0 low risk, 0.9 AI will take risks): ")
+
+# prompt="Write a creative ad for the following product to run on Facebook aimed at parents:\n\nProduct: Learning Room is a virtual environment to help students from kindergarten to high school excel in school."
+
+print("Loading...")
 
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt="Write a creative story for the following animal aimed at people adopting it:\n\nAnimal: Raccoon",
+  prompt=user_prompt,
   temperature=0.5,
   max_tokens=100,
   top_p=1.0,
